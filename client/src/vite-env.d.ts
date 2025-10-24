e238c2fb3b0651f19fc32d34e95fdadc8841e283
tanish0912 <tanish0912@users.noreply.github.com>
2025-10-24 22:24:16 +0530

    fixed env errors

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,4 +1,14 @@
 
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 declare module '*.svg' {
   const content: string
   export default content
